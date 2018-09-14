d0df5cec9c905f8e33718afce5fe307317b0c31d
wkwksama <mailpakgasing@gmail.com>
2018-09-14 19:35:36 +0700

    tes data js

---
+++ data/tealinuxos.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    name: 'TealinuxOS 9.0',
+    x86: 'http://pinguin.dinus.ac.id/iso/',
+    x64: 'http://pinguin.dinus.ac.id/iso/'
+  },
+  {
+    name: 'TealinuxOS 8.0',
+    x86: 'http://pinguin.dinus.ac.id/iso/',
+    x64: 'http://pinguin.dinus.ac.id/iso/'
+  },
+]
