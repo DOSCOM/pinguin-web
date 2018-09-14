139a28f5d59f63c446b8cc53a4b164e8106734df
wkwksama <mailpakgasing@gmail.com>
2018-09-14 13:36:01 +0700

    component layout

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,9 @@
 import React from "react"
-import { Link } from "gatsby"
-
-import Header from '../components/Header'
+import Layout from "../components/layout"
 
 export default () => (
-  <div style={{ color: `teal` }}>
-    <Link to="/contact/">Contact</Link>
-    <Header headerText="About Gatsby"/>
-    <Header headerText="It's pretty cool" />
-    <p>Such wow. Very React.</p>
-  </div>
+  <Layout>
+    <h1>About me</h1>
+    <p>I’m good enough, I’m smart enough, and gosh darn it, people like me!</p>
+  </Layout>
 )
