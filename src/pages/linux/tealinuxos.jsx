0552105665363ba119851582ae2218254d968576
wkwksama <mailpakgasing@gmail.com>
2018-09-15 15:56:18 +0700

    rename page jadi jsx

---
+++ src/pages/linux/tealinuxos.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../../components/Layout"
+
+export default () => (
+  <Layout>
+    <h1>Hi! TealinuxOS</h1>
+    <p>
+      What do I like to do? Lots of course but definitely enjoy building
+      websites.
+    </p>
+  </Layout>
+)
