85134d756e977bec2509d2663571805871260df1
wkwksama <mailpakgasing@gmail.com>
2018-09-15 13:17:55 +0700

    remove typography js

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,11 +3,5 @@ module.exports = {
     title: `Pandas Eating Lots`,
   },
   plugins: [
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography.js`,
-      },
-    },
   ],
 }
